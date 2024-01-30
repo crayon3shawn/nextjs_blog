b8990bd0ca4265bc5c48f4cdff86c56538b9e293
crayon3shawn <crayon3shawn@gmail.com>
2024-01-30 19:40:49 +0800

    更新 google oauth client id

--- config.client.ts
+++ config.client.ts
@@ -1,2 +1,2 @@
 export const GOOGLE_OAUTH_CLIENT_ID =
-  '552918956299-4jcjhv7c4m9e4r8cfbp4k3pugqfcgtk0.apps.googleusercontent.com';
+  '432401368510-3jkvroh9s1tig25b6gciipjttn0eds8h.apps.googleusercontent.com';
