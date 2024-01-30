2d44e17c3ee02142336a496fb39cb7150db07418
crayon3shawn <crayon3shawn@gmail.com>
2024-01-30 15:32:22 +0800

    Update SocialLinks.tsx

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,5 +1,5 @@
 import LineIcon from './LineIcon';
-import { Facebook, GitHub, Instagram } from 'react-feather';
+import { Facebook, GitHub, Globe, Instagram, Linkedin } from 'react-feather';
 
 interface Props {
   color?: string;
@@ -8,21 +8,39 @@ interface Props {
 function SocialLinks(props: Props) {
   return <div className="flex lg:mt-4 opacity-70 items-center">
     <a
+      href="https://portfolio.yuanlin.dev"
+      target="_blank"
+      rel="noreferrer">
+      <Globe
+        size={18}
+        className="transition hover:text-zinc-500"
+        color={props.color}/>
+    </a>
+    <a
       href="https://github.com/crayon3shawn"
       className="ml-4 transition hover:text-zinc-500"
       target="_blank"
       rel="noreferrer">
       <GitHub size={18} color={props.color}/>
     </a>
+    {/*
     <a
-      href="https://www.instagram.com/crayon3shawn/"
+      href="https://www.linkedin.com/in/yuanlinlin"
+      className="ml-4 transition hover:text-zinc-500"
+      target="_blank"
+      rel="noreferrer">
+      <Linkedin size={18} color={props.color}/>
+    </a>
+    */}
+    <a
+      href="https://www.instagram.com/cheng/"
       className="ml-4 transition hover:text-zinc-500"
       target="_blank"
       rel="noreferrer">
       <Instagram size={18} color={props.color}/>
     </a>
     <a
-      href="https://www.facebook.com/chengche0830"
+      href="https://www.facebook.com/yuaanlin"
       className="ml-4 transition hover:text-zinc-500"
       target="_blank"
       rel="noreferrer">
