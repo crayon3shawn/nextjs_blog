875143cd9d4847ddc490f0081f603f50be6ef628
crayon3shawn <crayon3shawn@gmail.com>
2024-01-30 15:23:54 +0800

    刪除多餘的socail link

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,5 +1,5 @@
 import LineIcon from './LineIcon';
-import { Facebook, GitHub, Globe, Instagram, Linkedin } from 'react-feather';
+import { Facebook, GitHub, Instagram } from 'react-feather';
 
 interface Props {
   color?: string;
@@ -8,27 +8,11 @@ interface Props {
 function SocialLinks(props: Props) {
   return <div className="flex lg:mt-4 opacity-70 items-center">
     <a
-      href="https://portfolio.yuanlin.dev"
-      target="_blank"
-      rel="noreferrer">
-      <Globe
-        size={18}
-        className="transition hover:text-zinc-500"
-        color={props.color}/>
-    </a>
-    <a
       href="https://github.com/crayon3shawn"
       className="ml-4 transition hover:text-zinc-500"
       target="_blank"
       rel="noreferrer">
       <GitHub size={18} color={props.color}/>
-    </a>
-    <a
-      href="https://www.linkedin.com/in/yuanlinlin"
-      className="ml-4 transition hover:text-zinc-500"
-      target="_blank"
-      rel="noreferrer">
-      <Linkedin size={18} color={props.color}/>
     </a>
     <a
       href="https://www.instagram.com/crayon3shawn/"
